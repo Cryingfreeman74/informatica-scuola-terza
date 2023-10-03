9c0414de5a0062a8fc6e134af9ff3605a2d4e682
Marco Balducci <balduccimarco2007@gmail.com>
2023-10-03 12:02:20 +0200

    aggiunto tp-sit

---
+++ TP-SIT terza/2023-09-20/ConsoleAppCodiceASetteSegmenti/ConsoleAppCodiceASetteSegmenti/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Marco Balducci 3H 2023/09/20
+ * Esercizio a pag 149 del libro: Display a 7 segmenti */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCodiceASetteSegmenti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear(); //il cursore si posiziona a riga 0 colonna 0
+
+            Console.WriteLine(""); //il cursore va a capo e passa alla riga successiva
+            Console.WriteLine("Digit display a 7 segmenti");
+
+            Console.WriteLine("");
+            Console.WriteLine("      ▓▓▓▓▓▓▓▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("      ▓▓▓▓▓▓▓▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("     ▓        ▓");
+            Console.WriteLine("      ▓▓▓▓▓▓▓▓");
+
+
+
+
+            Console.WriteLine("\nPremi un tasto per continuare");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TP-SIT terza/2023-09-27/InputDati/InputDati/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * 3H Balducci Marco 2023/09/27
+ * Esercizio che consiste nel ricevere dati in Input tramite console e verificare il tipo di dato letto.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputDati
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "3H Balducci Marco"; //Autore
+            //dichiarazione variabili input
+            string stInput;
+            bool inputOk;
+            int varInt; //intero letto
+
+            #region lettura Intero
+
+            do
+            {
+                Console.Write("Input valore intero -> ");
+                stInput = Console.ReadLine();
+
+                //conversione stringa in intero
+                inputOk = int.TryParse(stInput, out varInt); //converte la stringa in intero e se la conversione ha successo restituisce sia varInt che true, altrimenti falso
+                if (!inputOk) Console.WriteLine("Input non valido! Riprova");
+
+
+            } while (!inputOk); //ricicla se input non valido
+
+            #endregion
+
+            /*
+            #region lettura e stampa di una stringa
+
+            Console.Write("Input stringa -> ");
+            stInput = Console.ReadLine();
+
+            //Visualizzazione stringa ricevuta in input
+            Console.WriteLine("Stringa letta = " + stInput);
+
+            #endregion
+            */
+
+
+
+            #region Termine programma
+            Console.WriteLine("\nPremi un tasto per terminare il programma");
+            Console.ReadKey();
+            #endregion
+
+        }
+    }
+}
