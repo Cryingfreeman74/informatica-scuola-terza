41df22348fd3e344aebe3974d93a70b3475e4e20
Marco Balducci <balduccimarco2007@gmail.com>
2023-10-29 09:45:58 +0100

    added Molara Compiti

---
+++ Molara/ConsoleAppIndovinaMacchina/ConsoleAppIndovinaMacchina/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIndovinaMacchina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number, lowGuess = 0, highGuess = 100, guess = 50;
+            bool inputOk, guessed = false;
+            string strInput;
+            char charInput = ' ';
+            Random rnd = new Random();
+
+            Console.WriteLine("Pensa ad un numero da 1 a 100.\nIl programma proverà ad indovinare il numero e lei dovrà rispondere < se il suo numero è più piccolo, > se è + grande mentre = se indovina.\nQuando sei pronto premi un tasto.");
+            Console.ReadKey();
+
+            do
+            {
+                guess = rnd.Next(lowGuess, highGuess + 1);
+
+                Console.WriteLine("\nIl suo numero è per caso " + guess + " ?");
+                charInput = Console.ReadKey().KeyChar;
+
+                
+                switch (charInput)
+                {
+                    case '<':
+                        highGuess = guess;
+                        break;
+                    case '>':
+                        lowGuess = guess;
+                        break;
+                    case '=':
+                        guessed = true;
+                        break;
+                }
+
+            } while (!guessed);
+
+            Console.WriteLine("\ntrovato! il numero era " + guess);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Molara/ConsoleAppIndovinaMacchina/ConsoleAppMCD/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Esercizio1Cicli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stInput;
+            int firstNumber;
+            bool inputOk;
+            int secondNumber;
+
+            do
+            {
+                Console.Write("Inserire il primo numero da desidera calcolare -> ");
+                stInput = Console.ReadLine();
+
+                inputOk = int.TryParse(stInput, out firstNumber);
+
+                if (!inputOk) Console.WriteLine("Input non valido, riprova");
+                else if (firstNumber < 0)
+                {
+                    inputOk = false;
+                    Console.WriteLine("Input non valido, il numero deve essere positivo");
+                }
+
+            } while (!inputOk);
+
+            do
+            {
+                Console.Write("Inserire il secondo numero da desidera calcolare -> ");
+                stInput = Console.ReadLine();
+
+                inputOk = int.TryParse(stInput, out secondNumber);
+                if (!inputOk) Console.WriteLine("Input non valido, riprova");
+                else if (secondNumber < 0)
+                {
+                    inputOk = false;
+                    Console.WriteLine("Input non valido, il numero deve essere positivo");
+                }
+
+            } while (!inputOk);
+
+            
+
+            while (secondNumber != 0)
+            {
+                int temp = secondNumber;
+                secondNumber = firstNumber % secondNumber;
+                firstNumber = temp;
+            }
+
+            Console.Write($"Il Massimo Comune Divisore tra {firstNumber} e {secondNumber} è {firstNumber}");
+
+            Console.WriteLine("Premi un tasto per continuare");
+            Console.ReadKey();
+        }
+    }
+}
