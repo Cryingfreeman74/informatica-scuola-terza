de205fbd4081c1397daea90bf16d06a41dbbc38e
Marco Balducci <balduccimarco2007@gmail.com>
2023-10-29 09:35:59 +0100

    added EserciziSuiCicli2

---
+++ Molara/EserciziSuiCicli2/EserciziSuiCicli2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EserciziSuiCicli2
+{
+    internal class Program
+    {
+        static int ReadInput()
+        {
+            int number;
+
+            while (true)
+            {
+                Console.Write("Inserisci il primo fattore: ");
+                if (int.TryParse(Console.ReadLine(), out number)) break;
+                else
+                {
+                    Console.WriteLine("Input non valido, riprova");
+                    continue;
+                }
+
+            }
+
+            return number;
+        }
+
+        static bool IsPositive(int firstNumber, int secondNumber) {
+
+            if ((firstNumber >= 0 && secondNumber >= 0) || (firstNumber < 0 && secondNumber < 0)) return true; 
+            else return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            int firstFactor = ReadInput();
+            int secondFactor = ReadInput();
+
+            bool isPositive = IsPositive(firstFactor, secondFactor);
+
+            firstFactor = Math.Abs(firstFactor);
+            secondFactor = Math.Abs(secondFactor);
+
+            for(int i = 0; i < secondFactor; i++)
+            {
+                result += firstFactor;
+            }
+
+            if(!isPositive) result = -result;
+
+            Console.Write("Il risultato della moltiplicazione è: " + result);
+
+            Console.WriteLine("\n\nPremi un tasto per terminare il programma");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Molara/EserciziSuiCicli2/Quoziente/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoziente
+{
+    internal class Program
+    {
+        static int ReadInput(string messaggio)
+        {
+            int number;
+
+            while (true)
+            {
+                Console.Write(messaggio);
+                if (int.TryParse(Console.ReadLine(), out number)) break;
+                else
+                {
+                    Console.WriteLine("Input non valido, riprova");
+                    continue;
+                }
+
+            }
+
+            return number;
+        }
+
+        static bool IsPositive(int firstNumber, int secondNumber)
+        {
+
+            if ((firstNumber >= 0 && secondNumber >= 0) || (firstNumber < 0 && secondNumber < 0)) return true;
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+
+            int result = 0;
+            
+
+            int Dividendo = ReadInput("Inserisci il dividendo: ");
+            int Divisore = ReadInput("Inserisci il divisore: ");
+
+            bool isPositive = IsPositive(Dividendo, Divisore);
+
+            Dividendo = Math.Abs(Dividendo);
+            Divisore = Math.Abs(Divisore);
+
+            int temp = Dividendo;
+
+
+            while (temp >= Divisore)
+            {
+                temp -= Divisore;
+                result++;
+            }
+
+            int resto = Dividendo - Divisore * result;
+
+            if (!isPositive) result = -result;
+
+            Console.WriteLine("Il risultato della divisione è: " + result + "; Il resto è: " + resto);
+
+            Console.WriteLine("\n\nPremi un tasto per terminare il programma");
+            Console.ReadKey();
+
+        }
+    }
+}
