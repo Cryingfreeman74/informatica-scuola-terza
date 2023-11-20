985b1677f562ae07b1587aa7805ba754c61c3700
Marco Balducci <balduccimarco2007@gmail.com>
2023-11-20 12:58:51 +0100

    update

--- Sistemi-Reti/20-11-2023/for.sh
+++ Sistemi-Reti/20-11-2023/for.sh
@@ -4,7 +4,18 @@
 # arg rappresenta l'argomento che passiamo alla variabile, cioè assume il valore
 # di ognuna della variabili rappresentate nella lista
 
+# sintassi standard
+echo "Bene bambini, impariamo a contare fino a 5"
 for i in 1 2 3 4 5
 do
     echo "Numero $i"
+done
+
+# sintassi stile C
+LIMITE=10
+echo "Bene bambini, impariamo a contare fino a 5 in stile C"
+
+for ((i=1; i<=LIMITE; i++))
+do
+    #....
 done
