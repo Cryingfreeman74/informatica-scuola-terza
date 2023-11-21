5280e6a4bffc4116d4b16a614eda6fa49aba0bd4
Marco Balducci <balduccimarco2007@gmail.com>
2023-11-21 12:46:48 +0100

    added 21/11/23

---
+++ informatica/2023-11-21/ConteggioAllaRovescia/ConteggioAllaRovescia/Program.cs
@@ -0,0 +1,197 @@
+﻿//Marco Balducci 3H 2023-11-21
+//Programma a console che legge un numero e lo stampa rappresentadolo tramite caratteri di un display a 7 segmenti
+namespace ConteggioAllaRovescia
+{
+    internal class Program
+    {
+        static string[] cifra_0 = {
+            " ▓▓▓▓▓▓ ",
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static string[] cifra_1 = {
+            "       ▓",
+            "       ▓",
+            "       ▓",
+            "        ",
+            "       ▓",
+            "       ▓",
+            "       ▓",
+        };
+
+        static string[] cifra_2 = {
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+            "▓       ",
+            "▓       ",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static string[] cifra_3 = {
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static string[] cifra_4 = {
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            "       ▓",
+        };
+
+        static string[] cifra_5 = {
+            " ▓▓▓▓▓▓ ",
+            "▓       ",
+            "▓       ",
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static string[] cifra_6 = {
+            " ▓▓▓▓▓▓ ",
+            "▓       ",
+            "▓       ",
+            " ▓▓▓▓▓▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static string[] cifra_7 = {
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            "        ",
+            "       ▓",
+            "       ▓",
+            "       ▓",
+        };
+
+        static string[] cifra_8 = {
+            " ▓▓▓▓▓▓ ",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static string[] cifra_9 = {
+            " ▓▓▓▓▓▓ ",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static int getInt(string messaggio) //lettura intero
+        {
+            while (true)
+            {
+                Console.Write(messaggio);
+                if (int.TryParse(Console.ReadLine(), out int number)) return number;
+                else Console.WriteLine("Input non valido."); //gestione input non valido
+            }
+        }
+
+        static void StampaCifra(string[] cifra, int riga, int col) //stampa la cifra data
+        {
+            
+            for(int i = 0; i < cifra.Length; i++)
+            {
+                Console.SetCursorPosition(col, riga+i);
+                Console.WriteLine(cifra[i]);
+            }
+        }
+
+        static void StampaNumero(int numero, int riga) //stampa il numero
+        {
+            int col = 0;
+            string cifre = numero.ToString();
+            for(int i=0; i< cifre.Length; i++) //ciclo tra tutte le cifre del numero
+            {
+                switch (cifre[i])
+                {
+                    case '0':
+                        StampaCifra(cifra_0, riga, col);
+                        break;
+                    case '1':
+                        StampaCifra(cifra_1, riga, col);
+                        break;
+                    case '2':
+                        StampaCifra(cifra_2, riga, col);
+                        break;
+                    case '3':
+                        StampaCifra(cifra_3, riga, col);
+                        break;
+                    case '4':
+                        StampaCifra(cifra_4, riga, col);
+                        break;
+                    case '5':
+                        StampaCifra(cifra_5, riga, col);
+                        break;
+                    case '6':
+                        StampaCifra(cifra_6, riga, col);
+                        break;
+                    case '7':
+                        StampaCifra(cifra_7, riga, col);
+                        break;
+                    case '8':
+                        StampaCifra(cifra_8, riga, col);
+                        break;
+                    case '9':
+                        StampaCifra(cifra_9, riga, col);
+                        break;
+                }
+                col += 9; //spostamento a destra
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Marco Balducci 3H";
+            Console.WriteLine("marco balducci 3H");
+            /*
+            StampaCifra(cifra_0, 0, 0);
+            StampaCifra(cifra_1, 0, 8);
+            StampaCifra(cifra_2, 0, 17);
+            StampaCifra(cifra_3, 0, 26);
+            StampaCifra(cifra_4, 0, 35);
+            StampaCifra(cifra_5, 0, 44);
+            StampaCifra(cifra_6, 0, 53);
+            StampaCifra(cifra_7, 0, 61);
+            StampaCifra(cifra_8, 0, 70);
+            StampaCifra(cifra_9, 0, 79);
+            */
+
+            int number = getInt("inserisci il numero da rappresentare: ");
+
+            (int, int) position = Console.GetCursorPosition(); //ottengo la posizione di partenza
+            StampaNumero(number, position.Item2+1);
+
+            //termine programma
+            Console.WriteLine("Premi un tasto per terminare il programma");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ informatica/2023-11-21/pianoforte/pianoforte/Program.cs
@@ -0,0 +1,52 @@
+﻿//Marco Balducci 3H 2023-11-21
+//App a console che simula un pianoforte, ad un tasto premuto viene suonata la rispettiva nota
+namespace pianoforte
+{
+    internal class Program
+    {
+        // Indice -->               0    1    2    3    4    5    6    7    8    9    10   11
+        // Nota -->                 DO   RE   MI   FA   SOL  LA   SI  DO#  RE#  FA#  SOL#  LA#
+        static char[] keyboard = { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'w', 'e', 't', 'y', 'u' };
+        static int[] sound_freq = { 262, 294, 330, 349, 392, 440, 494, 277, 311, 370, 415, 466 };
+
+        static char leggiTasto() //input non bloccante
+        {
+            //torna il tasto premuto dall'utente, oppure '\0\' se non ci sono tasti premuti
+            if (!Console.KeyAvailable) //se non ci sono tasti premuti, torna immediatamente
+                return '\0';
+            ConsoleKeyInfo key = Console.ReadKey(true); //con intercept == true non c'è "echo" a video
+            return key.KeyChar;
+        }
+
+        static int FrequenzaNota(char nota) //ritorna la frequenza della nota se la trova, altrimenti 0
+        {
+            for (int i = 0; i < keyboard.Length; i++)
+            {
+                if (nota == keyboard[i]) return sound_freq[i];
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Marco Balducci 3H"; //autore
+            Console.WriteLine("Marco Balducci 3H\n");
+
+            Thread task = new Thread(() => { });
+
+            Console.WriteLine("Pianoforte! a w s e d f t g y h u j\nPremere q per terminare");
+            while (true) //ciclo del programma
+            {
+                char tasto = leggiTasto(); //lettura input
+
+                if (tasto != '\0') //se viene premuto qualcosa
+                {
+                    if (tasto == 'q') break;
+                    int frequenza = FrequenzaNota(tasto);
+
+                    if (frequenza != 0) Console.Beep(FrequenzaNota(tasto), 400); //se la frequenza è valida la suono
+                }
+
+            }
+        }
+    }
+}
