bd0ccdf9c5864cc3a6b16cd565ad2a3b68a5412d
Marco Balducci <balduccimarco2007@gmail.com>
2023-11-24 18:25:56 +0100

    added ex for 24/11/23

---
+++ Molara/SubString/SubString/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SubString
+{
+    internal class Program
+    {
+        static int searchFor(string mainString, string subString)
+        {
+            bool found = false;
+
+            for (int i = 0; i < mainString.Length-subString.Length+1; i++) 
+            {
+                for (int j = 0; j<subString.Length; j++)
+                {
+                    if (mainString[i+j] == subString[j]) found = true;
+                    else
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found) return i;
+            }
+            return -1;
+        }
+    }
+}
