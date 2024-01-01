7e492add4b0a4e5b19bcfa79f04634f14f53300d
Marco Balducci <balduccimarco2007@gmail.com>
2024-01-01 21:45:41 +0100

    changed test case

--- Molara/EserciziNatale23/FusioneVettori/Program.cs
+++ Molara/EserciziNatale23/FusioneVettori/Program.cs
@@ -25,7 +25,7 @@
         static void Main(string[] args)
         {
             int[] v1 = { 1, 5, 10, 15 };
-            int[] v2 = { 3, 5, 6, 12, 16, 17, 18, 19 };
+            int[] v2 = { 3, 5, 6, 7, 8, 9, 12, 16, 17, 18, 19 };
             int[] ordinato = UnisciOrdina(v1, v2);
 
             for (int i = 0; i < ordinato.Length;i++) Console.Write(ordinato[i] + " ");
