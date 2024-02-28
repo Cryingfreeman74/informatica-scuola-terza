07d5ccd680b2aa1c4db681d51a7ea7cbfc480f0a
Marco Balducci <balduccimarco2007@gmail.com>
2024-02-28 08:55:16 +0100

    added 28-02-2024 to tp-sit

---
+++ TP-SIT terza/2024-02-28/AsciiTable/AsciiTable/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Marco Balducci 3H 21-02-2024
+ * tabella valori ascii es da 0 a 127
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiTable
+{
+    internal class Program
+    {
+        static string getString(int i)
+        {
+            string result = "";
+
+            if (char.IsControl((char)i))
+            {
+                result = "Controllo";
+            }
+            else
+            {
+                if (char.IsWhiteSpace((char)i))
+                {
+                    result = "Spazio";
+                }
+                else
+                {
+                    result += (char)i;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int padlength = 10;
+            for (int i = 0; i < 32; i++)
+            {
+                    for(int j = 0; j < 4; j++)
+                    {
+                        int number = i + (j * 32);
+
+                        Console.Write($"{number.ToString().PadRight(3)} <--> {getString(i+(j*32)).PadRight(padlength)} | ");
+                    }
+                    Console.WriteLine();
+            }
+                Console.ReadKey();
+        }
+    }
+}
